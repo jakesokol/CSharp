370e2ecef9effc950c3a67d2d82d9017f7518b5f
Jake Sokol <sokoljake@gmail.com>
2022-07-13 16:04:11 -0600

    Create hangman.cs

---
+++ hangman.cs
@@ -0,0 +1,20 @@
+//Hangman Display
+using System;
+
+namespace Hangman
+{
+	class Program
+	{
+		static void Main(string[] args)
+
+		    { 
+				Console.WriteLine("( ^ ; ^ )");
+				Console.WriteLine("    |");
+				Console.WriteLine("    |");
+				Console.WriteLine("./  |  \\.");
+				Console.WriteLine("    |");
+				Console.WriteLine("  _/ \\_");
+				
+            }
+    }
+}

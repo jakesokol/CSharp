70c18a645a32e3b9df7a62e0fe26db5a42f84255
Jake Sokol <sokoljake@gmail.com>
2022-07-14 08:57:32 -0600

    Update RetailPriceofShirts.cs

--- RetailPriceofShirts.cs
+++ RetailPriceofShirts.cs
@@ -16,9 +16,9 @@ namespace RetailPriceofShirts
 			double netprice = wholesaleprice - discountapplied;
 
 			Console.WriteLine("Item: " + shirtname);
-            Console.WriteLine("Wholesale price of item: {0:C}" , wholesaleprice);
+            		Console.WriteLine("Wholesale price of item: {0:C}" , wholesaleprice);
 			Console.WriteLine("Discount applied: {0:C}" , discountapplied);
-            Console.WriteLine("Discount Rate:  {0:P}" ,  discountrate);
+            		Console.WriteLine("Discount Rate:  {0:P}" ,  discountrate);
 			Console.WriteLine("Net price of the item: {0:C}" , netprice);
 		}
 	}

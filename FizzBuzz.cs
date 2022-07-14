707335eb0b76c432e1ab13adf890d2c4ed1ec6b0
Jake Sokol <sokoljake@gmail.com>
2022-07-14 13:00:31 -0600

    Create FizzBuzz.cs

---
+++ FizzBuzz.cs
@@ -0,0 +1,40 @@
+/*
+ * Generate numbers 1-50 and output Fizz for multiples of 3, Buzz for Multiples of 5, and FizzBuzz for multiples of both 3 and 5. Output numbers for everything else
+ *
+ */
+
+namespace FizzBuzz
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ReadKey();
+			for (int i = 1; i <=50;i++)
+			{
+				bool buzzWord = false;
+				if (i % 3 == 0)
+				{
+					Console.WriteLine("Fizz");
+					buzzWord = true;
+				}
+				if (i % 5 == 0)
+                {
+					Console.WriteLine("Buzz");
+					buzzWord= true;
+				}
+				if	(i % 3 == 0 && i % 5 == 0)
+                {
+					Console.WriteLine("FizzBuzz");
+					buzzWord=(true);
+				}
+				if (!buzzWord)	
+                {
+					Console.WriteLine(i);
+                }
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
